7dc712f17b44d767fed5318bbe16bfbf33490738
Jan Skowronski <crianonim@gmail.com>
2019-08-09 11:57:24 +0100

    Query functions together with index

---
+++ src/queries/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  getAllDataQuery: require("./getAllDataquery"),
+  getSVGsQuery: require("./getSVGsquery"),
+  postSVGquery: require("./postSVGquery"),
+  postSHAPEquery: require("./postSHAPEquery"),
+  getSHAPEsquery: require("./getSHAPEsquery"),
+  insertSVG_SHAPEquery: require("./post_SVG_SHAPE")
+};

dd3bbc1bcbd13f529143e972477a7385df6c0ac1
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-08-15 15:16:51 +0100

    Modifies getAllDataquery

--- src/queries/getAllDataquery.js
+++ src/queries/getAllDataquery.js
@@ -1,13 +1,15 @@
-const db_connection=require('../../database/db_connection');
+const db_connection = require("../../database/db_connection");
 
-const getAllDataQuery=(cb)=>{
-    db_connection.query(`SELECT svg.props as svg_props ,svg.name AS svg_name,
-     type,shape.name AS shape_name,shape.props AS shape_props 
-    FROM svg JOIN svg_shape ON svg_shape.svg_id = svg.id JOIN shape ON shape.id = svg_shape.shape_id`,
-    (err,result)=>{
-        if (err) return err;
-        cb(result);
-    })
-}
+const getAllDataQuery = cb => {
+  db_connection.query(
+    `SELECT svg.props as svg_props ,svg.name AS svg_name,
+     type,shape.name AS shape_name,shape.props AS shape_props, username
+    FROM svg JOIN svg_shape ON svg_shape.svg_id = svg.id JOIN shape ON shape.id = svg_shape.shape_id JOIN login_details ON login_details.id = svg_shape.user_id`,
+    (err, result) => {
+      if (err) return err;
+      cb(result);
+    }
+  );
+};
 
 module.exports = getAllDataQuery;

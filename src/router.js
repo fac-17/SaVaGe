d3c7dfba860f9518bdab5c05aaeaf7c4b7e414cb
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-08-15 12:09:43 +0100

    Adds protected and unprotected route

--- src/router.js
+++ src/router.js
@@ -2,28 +2,39 @@ const handlers = require("./handlers");
 const log = require("./loggedIn");
 
 const router = (req, res) => {
-  if (req.url === "/") {
-    req.url = "/public/index.html";
-    const logged_In = log(req);
-  }
-  if (req.url.startsWith("/public")) {
-    handlers.staticAssets(req, res);
-  } else if (req.url === "/postSVG") {
-    handlers.postSVG(req, res);
-  } else if (req.url === "/getAllData") {
-    handlers.getAllData(req, res);
-  } else if (req.url === "/getSVGs") {
-    handlers.getSVGs(req, res);
-  } else if (req.url === "/postSHAPE") {
-    handlers.postSHAPE(req, res);
-  } else if (req.url === "/getSHAPEs") {
-    handlers.getSHAPEs(req, res);
-  } else if (req.url === "/insertSVG_SHAPE") {
-    handlers.insertSVG_SHAPE(req, res);
-  } else if (req.url === "/login") {
-    handlers.login(req, res);
+  const logged_In = log(req);
+  if (logged_In) {
+    //Protected Route
+    if (req.url === "/") {
+      req.url = "/public/indexProtected.html";
+    }
+    if (req.url.startsWith("/public")) {
+      handlers.staticAssets(req, res);
+    } else if (req.url === "/postSVG") {
+      handlers.postSVG(req, res);
+    } else if (req.url === "/getAllData") {
+      handlers.getAllData(req, res);
+    } else if (req.url === "/getSVGs") {
+      handlers.getSVGs(req, res);
+    } else if (req.url === "/postSHAPE") {
+      handlers.postSHAPE(req, res);
+    } else if (req.url === "/getSHAPEs") {
+      handlers.getSHAPEs(req, res);
+    } else if (req.url === "/insertSVG_SHAPE") {
+      handlers.insertSVG_SHAPE(req, res);
+    } else {
+      handlers.notFound(req, res);
+    }
   } else {
-    handlers.notFound(req, res);
+    //Unprotected Route
+    if (req.url === "/") {
+      req.url = "/public/indexUnprotected.html";
+    }
+    if (req.url.startsWith("/public")) {
+      handlers.staticAssets(req, res);
+    } else if (req.url === "/login") {
+      handlers.login(req, res);
+    }
   }
 };
 
